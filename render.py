12bba7c44f0388df6bf3e1b6f06bf823fc97e58b
cmdoret <cyril.mattheydoret@gmail.com>
2022-11-08 02:33:56 +0100

    add formatting logic for processors

--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 """Dynamically renders the readme from its jinja2 template and csv of assemblers"""
 import csv
 from dataclasses import dataclass
-from typing import List, Optional, Type
+from typing import Iterable, List, Optional, Type, TypeVar
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -33,12 +33,15 @@ class Assembler(Software):
 
 
 @dataclass
-class PostProcessor(Software):
+class Processor(Software):
     task: str
     reads: str
 
 
-def load_softwares(path: str, soft_type: Type[Software]) -> List[Software]:
+S = TypeVar("S", Software, Assembler, Processor)
+
+
+def load_softwares(path: str, soft_type: Type[S]) -> List[S]:
     """Load a bunch of softwares from CSV file"""
     softs = []
     with open(path, "r") as csvfile:
@@ -47,6 +50,37 @@ def load_softwares(path: str, soft_type: Type[Software]) -> List[Software]:
             # csv fields expand to dataclass attrs
             softs.append(soft_type(**row))
     return softs
+
+
+def fmt_processors(procs: Iterable[Processor]):
+    """Format processors so that:
+    + They are sorted by task, reads
+    + Reads field is in italic
+    + Only the first of each read type per task has a value
+    """
+    read_type = ""
+    task = ""
+    first = True
+    fmt_procs = sorted(procs, key=lambda x: (x.task, x.reads))
+
+    for pr in fmt_procs:
+        # First processor in category ?
+        if (pr.task != task) or (pr.reads != read_type):
+            first = True
+
+        read_type = pr.reads
+        task = pr.task
+
+        if not first:
+            pr.reads = ""
+        first = False
+
+    # Add italics
+    for pr in fmt_procs:
+        if pr.reads:
+            pr.reads = f"__{pr.reads}__"
+
+    return fmt_procs
 
 
 env = Environment(loader=FileSystemLoader("."), autoescape=False,)
@@ -68,6 +102,8 @@ print(template.render(assemblers=assemblers))
 ### PRE/POST PROCESSORS ###
 
 print("## Assembly pre and post-processing")
-procs = load_softwares("data/processors.csv", PostProcessor)
+procs = load_softwares("data/processors.csv", Processor)
 template = env.get_template("templates/processors.j2")
-print(template.render(processors=procs))
+# Gotta sort processors and edit them a bit for fancy md formatting
+fmt_procs = fmt_processors(procs)
+print(template.render(processors=fmt_procs))
