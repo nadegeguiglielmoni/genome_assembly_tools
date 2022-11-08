49cda41d1fcedc90cb1a1a3ff89b713d2c3921e3
cmdoret <cyril.mattheydoret@gmail.com>
2022-11-08 01:19:48 +0100

    fix paths and type annotations

--- render.py
+++ render.py
@@ -1,17 +1,19 @@
 """Renders the readme from its jinja2 template and csv of assemblers"""
 import csv
-from dataclass import dataclass
-from typing import List, Optional
+from dataclasses import dataclass
+from typing import List, Optional, Type
 
-import jinja2
 from jinja2 import Environment, FileSystemLoader
+
 
 @dataclass
 class Software:
     """Used to allow automatic update-date retrieval in subclasses"""
+
     name: str
-    publication: str
-    last_update: str
+    link: str
+    publication: Optional[str]
+    last_update: Optional[str]
 
     def __post_init__(self):
         if not self.last_update:
@@ -24,34 +26,34 @@ class Software:
         # http response header for others ? (unreliable)
         ...
 
+
 @dataclass
 class Assembler(Software):
     technology: str
+
 
 @dataclass
 class PostProcessor(Software):
     task: str
     reads: str
-    
 
-    
-def load_softwares(path: str) -> List[Software]:
+
+def load_softwares(path: str, soft_type: Type[Software]) -> List[Software]:
     """Load a bunch of softwares from CSV file"""
-    softs  = []
-    with open(path, 'r') as csvfile:
+    softs = []
+    with open(path, "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             # csv fields expand to dataclass attrs
-            softs.append(Assembler(**row))
+            print(row)
+            softs.append(soft_type(**row))
     return softs
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=True,
-)
+
+env = Environment(loader=FileSystemLoader("."), autoescape=True,)
 
 # Load list of assemblers and render template
-assemblers = load_assemblers('data/assemblers.csv')
-template = env.get_template('assemblers.tmpl')
+assemblers = load_softwares("data/assemblers.csv", Assembler)
+template = env.get_template("templates/assemblers.tmpl")
 print(template.render(assemblers=assemblers))
 
